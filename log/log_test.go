fbff8b9cd2680f3c1fcaa1ef1319f0e4af4738fd
sklee1216 <54343936+sklee1216@users.noreply.github.com>
2023-02-12 23:28:45 -0500

    Update log_test.go

--- log/log_test.go
+++ log/log_test.go
@@ -1,38 +1,14 @@
 package log
 
 import (
+	golog "log"
 	"os"
 	"testing"
 )
 
-func TestPrintln(t *testing.T) {
-	//test if TestPrintln is called
-	// setup
-	os.Setenv("LOG_LEVEL", "2")
-	os.Setenv("LOG_FILE", "./")
-
-	// tests
-	//print it to log file
-	Println("2", "this is a debug message")
-	Println("1", "this is an info message")
-	Println("0", "this is a none message")
-
-}
-
-func TestPrintf(t *testing.T) {
-	// setup
-	os.Setenv("LOG_LEVEL", "2")
-	os.Setenv("LOG_FILE", "./")
-
-	// tests
-	Printf("2", "this is a %s message", "debug")
-	Printf("1", "this is an %s message", "info")
-	Printf("0", "this is a %s message", "none")
-}
-
 func TestShouldLog(t *testing.T) {
 	// setup
-	os.Setenv("LOG_LEVEL", "2")
+	LOG_LEVEL = "2"
 
 	// tests
 	if !shouldLog("2") {
@@ -44,4 +20,19 @@ func TestShouldLog(t *testing.T) {
 	if shouldLog("0") {
 		t.Error("expected shouldLog(\"0\") to return false")
 	}
+
+}
+
+func TestNoLog(t *testing.T) {
+	LOG_LEVEL = "1"
+
+	f, _ := os.OpenFile(LOG_FILE, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+
+	defer f.Close()
+
+	golog.SetOutput(f)
+
+	Println(INFO, "Running Test Case (println)")
+	Printf(INFO, "Running Test Case (printf)")
+
 }
